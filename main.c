38cd4343284fdc7233c2a91816c02928de9b8b34
LeticiaCamelloBarros <lsncb@cesar.school>
2026-09-07 18:07:21 -0300

    criando lista de processos
    lista do tipo simplesmente encadeada

--- main.c
+++ main.c
@@ -7,4 +7,13 @@ if (argc != 3) {
         fprintf(stderr, "Uso: %s [algoritmo] [arquivo] \n", argv[0]);
         return EXIT_FAILURE;
     }
+    // ambos os algoritmos são preemptivos, uma tarefa de maior prioridade que chega ao sistema interrompe imediatamente a que está executando
+    if (argv[1] == "rate") {
+//...
+    }else if (argv[1]=="edf"){
+//..;
+    }else{
+        fprintf(stderr, "ERROR : o argumento %s não é o nome de um algoritmo válido", argv[1]);
+        return EXIT_FAILURE;
+    }
 }
--- utils.h
+++ utils.h
@@ -2,7 +2,7 @@ typedef struct PCB{
 //dados estáticos (lidos do arquivo de entrada)
 char nome[20];//nome da tarefa
 int periodo ;//periodo da tarefa
-int burst ; //Tempo de computação necessário
+int burst ; //Tempo de computação/execução necessário
 int deadline ;// Deadline relativo 
 int id_entrada ;//A ordem em que a tarefa apareceu no arquivo (crucial para aplicar a regra de desempate do projeto)
 // Dados Dinâmicos (atualizados a cada unidade de tempo na simulação)
@@ -10,3 +10,8 @@ int tempo_restante; //Quanto tempo de CPU a instância atual ainda precisa para 
 int proxima_chegada;// O próximo instante de tempo em que uma nova instância da tarefa vai chegar (com base no período $P$)
 int deadline_absoluto;// O tempo limite exato para a instância atual terminar. No EDF, a prioridade é definida por quem tem o menor deadline absoluto
 }PCB;
+
+typedef struct ready_queue {
+PCB processo ; 
+struct ready_queue *next ; 
+}ready;
