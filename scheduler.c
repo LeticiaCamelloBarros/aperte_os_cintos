dd0567b55256fa9fd46478afe84bb7c2692106c8
LeticiaCamelloBarros <lsncb@cesar.school>
2026-09-09 18:41:15 -0300

    seleção de prioridade de edf
    prioridade dinamica

--- scheduler.c
+++ scheduler.c
@@ -13,4 +13,13 @@ int prioridade_rate(TaskList *a, TaskList *b) {
         return a->tarefa.id_entrada < b->tarefa.id_entrada; /* desempate */
     return 0;
 }
+ /* EDF: prioridade dinamica, definida pelo deadline absoluto da instancia atual.
+ * Quanto MENOR o deadline absoluto, MAIOR a prioridade. */
+int prioridade_edf(TaskList *a, TaskList *b) {
+    if (a->tarefa.deadline_absoluto < b->tarefa.deadline_absoluto)
+        return 1;
+    if (a->tarefa.deadline_absoluto == b->tarefa.deadline_absoluto)
+        return a->tarefa.id_entrada < b->tarefa.id_entrada; /* desempate */
+    return 0;
+}
  
--- scheduler.h
+++ scheduler.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 #include "utils.h"
 int prioridade_rate(TaskList *a, TaskList *b) ;
+int prioridade_edf(TaskList *a, TaskList *b) ;
