a524b762070e7d6a10f752e172d852438d8c85bc
LeticiaCamelloBarros <lsncb@cesar.school>
2026-09-09 23:09:42 -0300

    corrigindo .h de utils

--- utils.h
+++ utils.h
@@ -3,9 +3,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-ready *inserir_pronta(ready *head, TaskList *alvo) ;
-//Instância de uma tarefa = cada nova ativação (chegada) daquela tarefa periódica.
-void liberar_cadastradas(TaskList *head) ;
 typedef struct {
     char nome[20];
     int periodo;
@@ -31,11 +28,10 @@ typedef struct ready_queue {
 TaskList *pcb ; 
 struct ready_queue *next ; 
 }ready;
+ready *inserir_pronta(ready *head, TaskList *alvo) ;
 ready *remover_pronta(ready *head, TaskList *alvo) ;
 void liberar_prontas(ready *head) ;
-ready *inserir_pronta(ready *head, TaskList *alvo) ;
 void liberar_cadastradas(TaskList *head) ;
 TaskList *inserir_tarefa(TaskList *head, Task t);
 int contar_tarefas(TaskList *head) ;
-
 #endif
